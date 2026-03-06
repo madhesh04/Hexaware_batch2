e8f6ba371b3b171fbd1a617b6278aaaab7d874e2
Madhesh P <123397037+madhesh04@users.noreply.github.com>
2026-03-06 16:38:57 +0530

    uploaded file via drag and drop

---
+++ demo.cs
@@ -0,0 +1,8 @@
+class Prg
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is a simple C# console application.");
+    }
+}
